10cf39a598252bbd7f5525e3ed4c905c708e9a4e
Brendan Dileo <dileob23@gmail.com>
2025-05-28 19:21:53 -0400

    Update pipeline

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -4,18 +4,50 @@ def call() {
   pipeline {
     agent any
 
-    stages {
-
-      stage('Test Print') {
-        steps {
-          echo "Running pipeline..."
-        }
-
-      }
-
+    environment {
+      IMAGE = 'brendanddev/node-app'
+      CONTAINER = 'node-app'
+      PORT = '3001'
     }
 
+    stages {
 
+      stage('Clean Workspace') {
+        steps {
+          deleteDir()
+        }
+      }
+
+      stage('Pull') {
+        steps {
+          sh 'docker pull $IMAGE'
+        }
+      }
+
+      stage('Run') {
+        steps {
+          sh '''
+            docker rm -f $CONTAINER_NAME || true
+            docker run -d --name $CONTAINER_NAME -p $PORT:$PORT $IMAGE_NAME
+            sleep 3
+          '''
+        }
+      }
+
+      stage('Test cURL') {
+        steps {
+          sh '''
+          echo "Testing app http://localhost:$PORT"
+          curl -f http://localhost:$PORT || exit 1
+          '''
+        }
+      }
+
+      stage('Last Clean') {
+        steps {
+          sh 'docker rm -f $CONTAINER || true'
+        }
+      }
+    }
   }
-
 }
