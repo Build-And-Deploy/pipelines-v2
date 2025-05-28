50ca301666a262f2b3abf43baa67e81d60451f21
Brendan Dileo <124395674+brendanddev@users.noreply.github.com>
2025-05-28 19:46:41 -0400

    Update testPipeline.groovy

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -4,12 +4,6 @@ def call() {
   pipeline {
     agent any
 
-    environment {
-      IMAGE = 'brendanddev/node-app'
-      CONTAINER = 'node-app'
-      PORT = '3001'
-    }
-
     stages {
 
       stage('Clean Workspace') {
@@ -18,36 +12,36 @@ def call() {
         }
       }
 
-      stage('Pull') {
-        steps {
-          sh 'docker pull $IMAGE'
-        }
-      }
+      // stage('Pull') {
+      //   steps {
+      //     sh 'docker pull $IMAGE'
+      //   }
+      // }
 
-      stage('Run') {
-        steps {
-          sh '''
-            docker rm -f $CONTAINER_NAME || true
-            docker run -d --name $CONTAINER_NAME -p $PORT:$PORT $IMAGE_NAME
-            sleep 3
-          '''
-        }
-      }
+      // stage('Run') {
+      //   steps {
+      //     sh '''
+      //       docker rm -f $CONTAINER_NAME || true
+      //       docker run -d --name $CONTAINER_NAME -p $PORT:$PORT $IMAGE_NAME
+      //       sleep 3
+      //     '''
+      //   }
+      // }
 
-      stage('Test cURL') {
-        steps {
-          sh '''
-          echo "Testing app http://localhost:$PORT"
-          curl -f http://localhost:$PORT || exit 1
-          '''
-        }
-      }
+      // stage('Test cURL') {
+      //   steps {
+      //     sh '''
+      //     echo "Testing app http://localhost:$PORT"
+      //     curl -f http://localhost:$PORT || exit 1
+      //     '''
+      //   }
+      // }
 
-      stage('Last Clean') {
-        steps {
-          sh 'docker rm -f $CONTAINER || true'
-        }
-      }
+      // stage('Last Clean') {
+      //   steps {
+      //     sh 'docker rm -f $CONTAINER || true'
+      //   }
+      // }
     }
   }
 }
